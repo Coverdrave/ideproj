71b78890877e7d3bf82c1956590df9a503c921f8
Coverdrave <coverdrave@abv.bg>
2024-11-19 16:23:04 +0200

    modified:   react-frontend/src/App.css 	modified:   react-frontend/src/App.jsx 	modified:   react-frontend/src/index.css

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [apiData, setApiData] = useState([])
+  
+  let hour = 7;
+  // let day = 0;
+
+
+  async function getApiData() {
+    const res = await fetch('/api/schedule');
+    const data = await res.json();
+
+    if (res.ok) {
+      setApiData(data);
+    }
+  }
+
+  useEffect(() => {
+    getApiData();
+    console.log(apiData);
+  }, []);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='mx-auto text-center'>Учебен разпис</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Ден</th>
+            <th>гр.</th>
+            <th>7:00 - 7:45</th>
+            <th>8:00 - 8:45</th>
+            <th>9:00 - 9:45</th>
+            <th>10:00 - 10:45</th>
+            <th>гр.</th>
+            <th>гр.</th>
+            <th>гр.</th>
+            <th>гр.</th>
+            <th>гр.</th>
+            <th>гр.</th>
+            <th>гр.</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {
+              
+            }
+            <td>
+              {apiData
+
+              }
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </>
   )
 }
-
-export default App
